6d48b05d160c0869e50d0a2a0f1a1e2178fe8fc3
Adrian Ramirez <adrram0202@gmail.com>
2023-10-27 13:18:48 -0500

    Create result and home pages, create routes, working on converting CSV

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -14,6 +14,82 @@ const template = path.join(__dirname, 'templates');
 let app = express();
 app.use(express.static(root));
 
+const db = new sqlite3.Database(path.join(__dirname, 'sleeping-alone.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        console.log('Database read error');
+    } else {
+        console.log('Fully connected to database');
+    }
+});
+
+// Homepage display
+app.get('/', (req, res) => {
+    // Get file path to index.html
+    let filepath = path.join(template, 'index.html');
+
+    fs.readFile(filepath, "utf-8", (err, data) => {
+        console.log(filepath);
+        if (err) {
+            console.log('Home Read Error');
+            res.status(404).type('txt').send('File not found');
+        } else {
+            console.log('Home Read Success');
+            res.status(200).type('html').send(data);
+        }
+    });
+})
+
+// Route 1: Relationship Status
+app.get('/rel/:rel-status/:entry', (req, res) => {
+    // Get file path to index.html
+    let filepath = path.join(template, 'temp.html');
+
+    fs.readFile(filepath, "utf-8", (err, data) => {
+        console.log(filepath);
+        if (err) {
+            console.log('Relation Read Error');
+            res.status(404).type('txt').send('File not found');
+        } else {
+            console.log('Relation Read Success');
+            res.status(200).type('html').send(data);
+        }
+    });
+});
+
+// Route 2: Gender
+app.get('/gdr/:gender/:entry', (req, res) => {
+    // Get file path to index.html
+    let filepath = path.join(template, 'temp.html');
+
+    fs.readFile(filepath, "utf-8", (err, data) => {
+        console.log(filepath);
+        if (err) {
+            console.log('Gender Read Error');
+            res.status(404).type('txt').send('File not found');
+        } else {
+            console.log('Gender Read Success');
+            res.status(200).type('html').send(data);
+        }
+    });
+});
+
+// Route 3: Location
+app.get('/loc/:location/:entry', (req, res) => {
+    // Get file path to index.html
+    let filepath = path.join(template, 'temp.html');
+
+    fs.readFile(filepath, "utf-8", (err, data) => {
+        console.log(filepath);
+        if (err) {
+            console.log('Location Read Error');
+            res.status(404).type('txt').send('File not found');
+        } else {
+            console.log('Location Read Success');
+            res.status(200).type('html').send(data);
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
